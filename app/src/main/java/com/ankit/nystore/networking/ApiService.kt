c99b6dfce251287c35cbfb1e27b4cb8c41101ac7
Ankit <apsaliya@gmail.com>
2018-04-23 14:34:25 +0530

    Remove binding dependency and add a todo.

--- app/src/main/java/com/ankit/nystore/networking/ApiService.kt
+++ app/src/main/java/com/ankit/nystore/networking/ApiService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
+  //TODO Create custom Retrofit Converter to directly get Flowable<List<Story>>
   @GET("topstories/v2/home.json")
   fun getTopStories(@Query("api-key") apiKey: String): Flowable<TopStories>
 }
